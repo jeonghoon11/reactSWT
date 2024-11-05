8b5870f73244023d08dc432722e3f6432a542870
jeonghoon11 <jih361@naver.com>
2024-11-05 21:36:34 +0900

    Head 폴더 생성

--- swt/src/App.js
+++ swt/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Contect from './component/Contect';
-import Header from './component/Header';
+import Contect from './component/Head/Contect';
+import Header from './component/Head/Header';
 import {  BrowserRouter, Route, Routes  } from 'react-router-dom';
-import InfoShare from './component/InfoShare';
-import Member from './component/Memeber';
-import Q_A from './component/Q_A';
-import BaekJoon from './component/BaekJoon';
+import InfoShare from './component/Head/InfoShare';
+import Member from './component/Head/Memeber';
+import Q_A from './component/Head/Q_A';
+import BaekJoon from './component/Head/BaekJoon';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Routes>
             <Route path="/Contect" element={<Contect />} />
             <Route path="/InfoShare" element={<InfoShare />} />
-            <Route path="/Member" element={<Member />} />
+            <Route path="/Member " element={<Member />} />
             <Route path="/Q_A" element={<Q_A />} />
             <Route path="/BaekJoon" element={<BaekJoon />} />
         </Routes>
