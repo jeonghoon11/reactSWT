416223bc88a78839eb9f70d961a002940adef4d7
jeonghoon11 <jih361@naver.com>
2024-11-05 22:01:47 +0900

    컴포넌트 이동

--- swt/src/App.js
+++ swt/src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
-import Contect from './component/Head/Contect';
 import Header from './component/Head/Header';
-import {  BrowserRouter, Route, Routes  } from 'react-router-dom';
-import InfoShare from './component/Head/InfoShare';
-import Member from './component/Head/Memeber';
-import Q_A from './component/Head/Q_A';
-import BaekJoon from './component/Head/BaekJoon';
+import {  BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <Header />
-        <Routes>
-            <Route path="/Contect" element={<Contect />} />
-            <Route path="/InfoShare" element={<InfoShare />} />
-            <Route path="/Member " element={<Member />} />
-            <Route path="/Q_A" element={<Q_A />} />
-            <Route path="/BaekJoon" element={<BaekJoon />} />
-        </Routes>
       </div>
     </BrowserRouter>
   );
--- swt/src/component/Head/Header.js
+++ swt/src/component/Head/Header.js
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
+import Contect from './Contect'
+import InfoShare from './InfoShare';
+import Member from './Member';
+import Q_A from './Q_A';
+import BaekJoon from './BaekJoon';
 import Nav from "./Nav";
 
 export default function Header() {
     return (
-        <div className="header">
+        <div>
+            <div className="header">
             <h1 className="title">
                 <Link to="/">Study_Web</Link>
             </h1>
             <Nav />
+            </div>
+            <div>
+                <Routes>
+                    <Route path="/Contect" element={<Contect />} />
+                    <Route path="/InfoShare" element={<InfoShare />} />
+                    <Route path="/Member " element={<Member />} />
+                    <Route path="/Q_A" element={<Q_A />} />
+                    <Route path="/BaekJoon" element={<BaekJoon />} />
+                </Routes>
+            </div>
         </div>
     );
 }
