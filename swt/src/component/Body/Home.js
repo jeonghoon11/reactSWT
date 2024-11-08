97fa2ecd933cf296eef98869f479bb11affdd1ab
jeonghoon11 <jih361@naver.com>
2024-11-08 23:28:51 +0900

    login 틀잡기

--- swt/src/component/Body/Home.js
+++ swt/src/component/Body/Home.js
@@ -1,7 +1,18 @@
+import userIcon from './img/userIcon.png';
 export default function Home() {
     return (
-        <div className="main-body">
-             
+        <div className="home-body">
+            <form className="container">
+                <h1 className="login-box">Login</h1>
+                <section className="username-box">
+                    <img src={userIcon} className='user-icon'/>
+                    <input type="text" name="username" placeholder="     Username" />
+                </section>
+                <section className="passwd-box">
+                    <img src={userIcon} className='user-icon'/>
+                    <input type="text" name="passwd" placeholder="      Passwd" />
+                </section>
+            </form>
         </div>
     );
 }
