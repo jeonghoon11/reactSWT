e699925dc5890c7f2b1a004a582a48299045f500
jeonghoon11 <jih361@naver.com>
2024-11-10 01:30:49 +0900

    Login컴포넌트 생성

---
+++ swt/src/component/Body/Login.js
@@ -0,0 +1,19 @@
+import userIcon from './img/userIcon.png';
+
+export default function Login() {
+    return (
+        <div className="home-body">
+            <form className="container">
+                <h1 className="login-box">Login</h1>
+                <section className="username-box">
+                    <img src={userIcon} className='user-icon'/>
+                    <input type="text" name="username" placeholder="     Username" />
+                </section>
+                <section className="passwd-box">
+                    <img src={userIcon} className='user-icon'/>
+                    <input type="text" name="passwd" placeholder="      Passwd" />
+                </section>
+            </form>
+        </div>
+    );
+}
