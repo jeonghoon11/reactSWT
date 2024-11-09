1ba46175f23ea3150a4aec424b55be438f7517d6
jeonghoon11 <jih361@naver.com>
2024-11-10 01:31:32 +0900

    className 변경

--- swt/src/component/Head/Nav.js
+++ swt/src/component/Head/Nav.js
@@ -3,19 +3,19 @@ import Find from './find.png';
 
 export default function Nav() {
     return (
-        <ul className="nav">
-            <li><Link to="/Contect">Contect</Link></li>
-            <li><Link to="/InfoShare">InfoShare</Link></li>
-            <li><Link to="/Member">Member</Link></li>
-            <li><Link to="/BaekJoon">BaekJoon</Link></li>
-            <li>    
-                <div className='text-box'>
-                    <input type="text" placeHolder="물어보세용" className="Text"></input>
-                    <button className="find-button">
-                        <img src={Find} className='find-icon'></img>
-                    </button>
-                </div>  
-            </li>
-        </ul>
+        <div className="nav-body">
+            <ul className="nav">
+                <li><Link to="/Contect">Contect</Link></li>
+                <li><Link to="/InfoShare">InfoShare</Link></li>
+                <li><Link to="/Member">Member</Link></li>
+                <li><Link to="/BaekJoon">BaekJoon</Link></li>  
+            </ul>
+            <div className='text-box'>
+                <input type="text" placeHolder="물어보세용" className="Text"></input>
+                <button className="find-button">
+                    <img src={Find} className='find-icon'></img>
+                </button>
+            </div>
+        </div>
     )
 }
