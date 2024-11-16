e81ec0f4a73b9c6e978e400ac00d3d0aa2dc8ac4
jeonghoon11 <jih361@naver.com>
2024-11-17 01:37:48 +0900

    툴 생성

---
+++ swt/src/component/Body/Tools/Tool.js
@@ -0,0 +1,9 @@
+import Todo from "./TodoList"
+import Calculator from "./Calculator"
+export default function Tool () {
+    return (
+        <div>
+            <Calculator />
+        </div>
+    )
+}
