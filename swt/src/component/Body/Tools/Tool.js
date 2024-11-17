6cd706a86038a37c793a407fcd5b5921aa9ba2f1
jeonghoon11 <jih361@naver.com>
2024-11-18 00:59:16 +0900

    Tool에 TodoList 추가

--- swt/src/component/Body/Tools/Tool.js
+++ swt/src/component/Body/Tools/Tool.js
@@ -1,9 +1,12 @@
-import Todo from "./TodoList"
-import Calculator from "./Calculator"
+import Todo from "./TodoList";
+import Calculator from "./Calculator";
+import styles from "./Tool.module.css"
+
 export default function Tool () {
     return (
-        <div>
+        <div className={styles.container}>
             <Calculator />
+            <Todo />
         </div>
     )
 }
