53a9a0270a44922790b2552d205de68c1eda88c9
jeonghoon11 <jih361@naver.com>
2024-11-18 00:58:55 +0900

    Create TodoList

--- swt/src/component/Body/Tools/TodoList.js
+++ swt/src/component/Body/Tools/TodoList.js
@@ -1,7 +1,29 @@
+import styles from "./TodoList.module.css";
+import { useState } from "react";
+
 export default function TodoList() {
+    const [init, setInit] = useState("");
+    const [list,setList] = useState([]);
+
     return (
-        <div>
-            1
+        <div className={styles.container}>
+            <div className={styles.title}>
+                Note.
+            </div>
+            <div className={styles.listCreatBox}>
+                <input type="text" className={styles.listBox} placeholder="오늘은 무엇을 하시나요?" />
+            </div>
+            <div>
+                <ul className={styles.none}>
+                    <li className={styles.textContainer}>
+                        <label className={styles.list}>
+                            <input type="checkbox" />
+                            <span>Sample List item</span>
+                        </label>
+                        <button className={styles.list_botton}>X</button>
+                    </li>
+                </ul>
+            </div>
         </div>   
     )
 }
